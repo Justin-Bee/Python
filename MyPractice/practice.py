08f00722d51e1fd09ac64c48ffb06e699c4e78df
jbee.appz@gmail.com <jbee.appz@gmail.com>
2025-03-09 16:27:05 -0400

    more practice

---
+++ MyPractice/practice.py
@@ -0,0 +1,14 @@
+import pandas as pd
+name = ""
+
+while name != "exit":
+    name = input("please enter a file name or type exit to quit")
+    print(name)
+    if name == "exit":
+        break
+    try:    
+        df = pd.read_csv(name)
+    except:
+        print("file was not found") 
+        break   
+    print(df.columns)
